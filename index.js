b14e0bffa04c409c0cf3cd757c020cee43907762
kevinsvni <kvnsavani@gmail.com>
2023-10-26 16:29:42 -0400

    NodeJSTutoral 14 - Import Export Patterns

--- index.js
+++ index.js
@@ -4,13 +4,26 @@
 // console.log(addFn(1,4))
 // console.log(addFn(4,4))
 
-const SuperHero = require("./super-hero");
 
-const batman = new SuperHero("Batman");
-console.log(batman.getName());
 
-batman.setName("Bruce Wayne");
-console.log(batman.getName());
 
-const superman = new SuperHero("Superman");
-console.log(superman.getName());
+
+// const SuperHero = require("./super-hero");
+
+// const batman = new SuperHero("Batman");
+// console.log(batman.getName());
+
+// batman.setName("Bruce Wayne");
+// console.log(batman.getName());
+
+// const superman = new SuperHero("Superman");
+// console.log(superman.getName());
+
+
+
+
+
+const math = require("./moduleMath")
+// const {add, sub} = math
+console.log(math.add(1, 2));
+console.log(math.sub(1, 2));
