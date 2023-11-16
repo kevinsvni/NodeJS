9f5886895b8422bc7aa0a6167e174971b280d702
kevinsvni <kvnsavani@gmail.com>
2023-11-16 13:09:34 -0500

    NodeJSTutoral 26 - fs Module

---
+++ fs/index.js
@@ -0,0 +1,28 @@
+const fs = require("node:fs");
+
+console.log("First");
+
+const data = fs.readFileSync("../file.txt", "utf-8");
+console.log(data);
+
+console.log("Second");
+
+fs.readFile("../file.txt", "utf-8", (error, data) => {
+    if(error){
+        console.log(error)
+    }else{
+        console.log(data)
+    }
+})
+
+console.log("Third");
+
+fs.writeFileSync("./greet.txt", "Hello Kevin!");
+
+fs.writeFile("greet.txt", " \n\nHello World!!!", {flag: "a"}, (err) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("File written successfully")
+    }
+})
