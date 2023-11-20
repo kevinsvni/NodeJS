f24414069d590909ddfb1d265ca964be8fd168dd
kevinsvni <kvnsavani@gmail.com>
2023-11-20 11:59:41 -0500

    NodeJSTutoral 40 - Thread Pool Size

--- threadpool/index.js
+++ threadpool/index.js
@@ -1,7 +1,9 @@
 const crypto = require("node:crypto");
 
+process.env.UV_THREADPOOL_SIZE = 4
+
 const start = Date.now();
-for(let i=0; i<5; i++){
+for(let i=0; i<4; i++){
     crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
         console.log(`Hash: ${i+1}`, Date.now() - start);
     });
