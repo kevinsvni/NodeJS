6881412f0f0e07dccde95ea88ba0097b4a5c6101
kevinsvni <kvnsavani@gmail.com>
2023-11-20 10:28:59 -0500

    NodeJSTutoral 34 - HTML Template

--- htmlserver/index.js
+++ htmlserver/index.js
@@ -3,9 +3,16 @@ const fs = require("node:fs")
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Contect-Type": "text/html"});
+
     // const data =fs.readFileSync("./index.html");
     // res.end(data);
-    fs.createReadStream(__dirname + "/index.html").pipe(res);
+
+    // fs.createReadStream(__dirname + "/index.html").pipe(res);
+
+    const name = "Kevin";
+    let data = fs.readFileSync("./index.html", "utf-8");
+    data = data.replace("{{name}}", name);
+    res.end(data);
 })
 
 server.listen(8888, () => {
